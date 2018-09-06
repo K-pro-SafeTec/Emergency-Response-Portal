1f01802f35a6e4cb550f75be49a80084c778af14
MariaSoleim <maria@soleim.com>
2018-09-06 15:59:31 +0200

    Possible to click the add-button

---
+++ src/components/AddEvent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class AddEvent extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hei</h1>
+      </div>
+    )
+  }
+}
--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+import { AddEvent } from "./AddEvent";
 import BigCalendar from 'react-big-calendar';
 import moment from 'moment';
 import 'moment/locale/nb';
@@ -7,14 +8,27 @@ import 'moment/locale/nb';
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 import '../styles/Calendar.css'
 
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+
 
 moment.locale('nb');
 BigCalendar.momentLocalizer(moment);
 
 export class EmergencyResponsePortalCalendar extends Component {
   
-  dayClicked(value) {
-    alert("Day clicked: " + value)
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: [],
+      showEventAdder: false,
+    };
+  }
+  
+  addButtonClicked() {
+    this.setState({
+      showEventAdder: true
+    })
   }
   
   render() {
@@ -22,8 +36,40 @@ export class EmergencyResponsePortalCalendar extends Component {
       <div className="container">
         {/*<ApiCalendar/>*/}
         <BigCalendar
-          events={[]}
+          events={[
+            {
+              start: new Date('2018-09-07T21:45:00.000Z'),
+              end: new Date('2018-09-07T21:45:00.000Z'),
+              type: 'Øvelse',
+              participants: 'Alle',
+              title: 'Øvelse',
+            },
+            {
+              end: new Date('2018-09-08T21:45:00.000Z'),
+              start: new Date('2018-09-08T21:45:00.000Z'),
+              type: 'Øvelse',
+              participants: 'Alle',
+              title: 'Trening',
+            },
+            {
+              end: new Date('2018-09-09T21:45:00.000Z'),
+              start: new Date('2018-09-09T21:45:00.000Z'),
+              type: 'Øvelse',
+              participants: 'Alle',
+              title: 'Table Top',
+            },
+          ]}
+          onSelectEvent={({ participants }) => console.log(participants)}
+
         />
+        <div onClick={() => this.addButtonClicked()}>
+          <Button variant="fab" color="primary">
+            <AddIcon />
+          </Button>
+        </div>
+        {this.state.showEventAdder && (
+          <AddEvent/>
+        )}
       </div>
     )
   }
