6edcab5a531ab2bb496c4c18e5a175cb859a9974
MariaSoleim <maria@soleim.com>
2018-09-06 12:37:03 +0200

    Using BigCalendar

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
 // import ApiCalendar from 'react-google-calendar-api';
-import Calendar from 'react-calendar'
+// import Calendar from 'react-calendar'
 // import Calendar from 'react-calendar/dist/entry.nostyle';
 import '../styles/Calendar.css'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
+
+BigCalendar.setLocalizer(
+  BigCalendar.momentLocalizer(moment)
+);
 
 export class EmergencyResponsePortalCalendar extends Component {
   
-  dayClicked() {
-    alert("Day clicked")
+  dayClicked(value) {
+    alert("Day clicked: " + value)
   }
   
   render() {
     return (
       <div className="container">
         {/*<ApiCalendar/>*/}
-        <Calendar onClickDay={() => this.dayClicked()}/>
+        <BigCalendar
+          events={[]}
+        />
       </div>
     )
   }
