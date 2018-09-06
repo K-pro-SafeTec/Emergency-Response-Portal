3699e0ce9e441f4e4c08d3550d07885d0c075251
MariaSoleim <maria@soleim.com>
2018-09-06 13:21:51 +0200

    Norwegian

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 
 import BigCalendar from 'react-big-calendar';
 import moment from 'moment';
-import 'moment/locale/en-gb';
+import 'moment/locale/nb';
 
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 import '../styles/Calendar.css'
 
 
-moment.locale('en-gb');
+moment.locale('nb');
 BigCalendar.momentLocalizer(moment);
 
 export class EmergencyResponsePortalCalendar extends Component {
