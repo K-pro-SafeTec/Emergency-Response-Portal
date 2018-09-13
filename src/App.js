0c728b2ae5e27205933080913deb567925bbbe03
Erlend Åmdal <erlend@aamdal.com>
2018-09-13 10:02:22 +0200

    Add initial livefeed component and display in app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import MainMenu from './components/main-menu-components/MainMenu';
 import { EmergencyResponsePortalCalendar as Calendar } from './components/calendar-components/Calendar';
+import Livefeed from './components/livefeed-components/Livefeed';
 
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
         <React.Fragment>
           <Route exact path="/" component={MainMenu} />
           <Route path="/calendar" component={Calendar} />
+          <Route path="/livefeed" component={Livefeed} />
         </React.Fragment>
       </Router>
     );
