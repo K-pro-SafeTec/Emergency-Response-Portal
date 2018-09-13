588d3a6e406f2e7ebc96fafaf5fd20342f8baa5f
Erlend Åmdal <erlend@aamdal.com>
2018-09-13 14:07:15 +0200

    Add MainMenu

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import MainMenu from './components/main-menu-components/MainMenu';
 import { EmergencyResponsePortalCalendar as Calendar } from './components/calendar-components/Calendar';
 
-const Nav = () => (
-  <ul>
-    <li>
-      <Link to="/calendar">Calendar</Link>
-    </li>
-  </ul>
-);
 
 class App extends Component {
   render() {
     return (
       <Router>
         <React.Fragment>
-          <Route exact path="/" component={Nav} />
+          <Route exact path="/" component={MainMenu} />
           <Route path="/calendar" component={Calendar} />
         </React.Fragment>
       </Router>
