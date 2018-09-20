9ee5ed8e4e1f1663c80ace3e7cfa1e0263927632
Erlend Åmdal <erlend@aamdal.com>
2018-09-20 10:50:11 +0200

    Remove title from MainMenu

--- src/components/main-menu/MainMenu.js
+++ src/components/main-menu/MainMenu.js
@@ -8,22 +8,18 @@ import IconShield from '../../icons/shield.svg';
 
 
 export default () => (
-  <div>
-
-    <h1 className="title">Emergency Response Portal</h1>
-    <ul className="container">
-      <Link className="element" to="/calendar">
-        <img className="icon" src={IconCalendar} alt="Kalender"/>
-        <h3>Kalender</h3>
-      </Link>
-      <Link className="element" to="/livefeed">
-        <img className="icon" src={IconLivefeed} alt="Livefeed"/>
-        <h3>Livefeed</h3>
-      </Link>
-      <Link className="element" to="/emergency-response-organization">
-        <img className="icon" src={IconShield} alt="Beredskapsorganisasjon"/>
-        <h3>Beredskapsorganisasjon</h3>
-      </Link>
-    </ul>
-  </div>
+  <ul className="container">
+    <Link className="element" to="/calendar">
+      <img className="icon" src={IconCalendar} alt="Kalender"/>
+      <h3>Kalender</h3>
+    </Link>
+    <Link className="element" to="/livefeed">
+      <img className="icon" src={IconLivefeed} alt="Livefeed"/>
+      <h3>Livefeed</h3>
+    </Link>
+    <Link className="element" to="/emergency-response-organization">
+      <img className="icon" src={IconShield} alt="Beredskapsorganisasjon"/>
+      <h3>Beredskapsorganisasjon</h3>
+    </Link>
+  </ul>
 );
