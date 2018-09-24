e953629fe2b96fe66f5ad0b79d59d54f7e899425
Erlend Åmdal <erlend@aamdal.com>
2018-09-24 13:21:45 +0200

    Add competence overview team page

--- src/components/competence-overview/CompetenceOverview.js
+++ src/components/competence-overview/CompetenceOverview.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Route, Switch } from 'react-router-dom';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import StatusIcon from '../shared/StatusIcon';
 import AppPage from '../shared/AppPage';
+import Team from './Team';
 import { teamList } from '../../dummy-data/team';
 
 const TeamItem = ({ team }) => (
-  <ListItem button component={Link} to={`team/${team.id}/`}>
+  <ListItem button component={Link} to={`teams/${team.id}/`}>
     <ListItemIcon style={{ fontSize: '40px' }}>
       <StatusIcon status={team.status} />
     </ListItemIcon>
@@ -17,12 +18,17 @@ const TeamItem = ({ team }) => (
   </ListItem>
 );
 
-export default () => (
-  <AppPage title="Kompetanseoversikt">
-    <List>
-      {teamList.map(team => (
-        <TeamItem key={team.id} team={team} />
-      ))}
-    </List>
-  </AppPage>
+export default ({ match }) => (
+  <Switch>
+    <Route exact path={match.path}>
+      <AppPage title="Kompetanseoversikt">
+        <List>
+          {teamList.map(team => (
+            <TeamItem key={team.id} team={team} />
+          ))}
+        </List>
+      </AppPage>
+    </Route>
+    <Route path={`${match.url}/teams/:teamId/`} component={Team} />
+  </Switch>
 );
--- src/components/competence-overview/Team.js
+++ src/components/competence-overview/Team.js
@@ -6,27 +6,30 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import StatusIcon from '../shared/StatusIcon';
+import AppPage from '../shared/AppPage';
 
 export default ({ match }) => {
   const team = teamById[match.params.teamId];
   return team ? (
-    <List>
-      {team.members.map(memberId => {
-        const person = personById[memberId];
-        return person ? (
-          <ListItem key={memberId}>
-            <ListItemIcon style={{ fontSize: '40px' }}>
-              <StatusIcon status={team.status} />
-            </ListItemIcon>
-            <ListItemText primary={person.name} />
-          </ListItem>
-        ) : (
-          <ListItem key={memberId}>
-            <ListItemText>Ukjent person</ListItemText>
-          </ListItem>
-        );
-      })}
-    </List>
+    <AppPage title={team.name}>
+      <List>
+        {team.members.map(memberId => {
+          const person = personById[memberId];
+          return person ? (
+            <ListItem key={memberId} button>
+              <ListItemIcon style={{ fontSize: '40px' }}>
+                <StatusIcon status={team.status} />
+              </ListItemIcon>
+              <ListItemText primary={person.name} />
+            </ListItem>
+          ) : (
+            <ListItem key={memberId}>
+              <ListItemText>Ukjent person</ListItemText>
+            </ListItem>
+          );
+        })}
+      </List>
+    </AppPage>
   ) : (
     <p>Fant ikke noe lag med denne IDen.</p>
   );
