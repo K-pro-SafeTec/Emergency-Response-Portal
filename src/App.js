152bfd086ba5fd4bf990f936af28759aa79126a3
Erlend Åmdal <erlend@aamdal.com>
2018-09-24 12:50:37 +0200

    Fix module name casing

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import './App.css';
 import MainMenu from './components/main-menu/MainMenu';
 import Calendar from './components/calendar/Calendar';
 import Livefeed from './components/livefeed/Livefeed';
-import ExperienceLog from './components/experience-log/experienceLog.js';
-import Excercises from './components/exercise/exercise.js';
-import ExcerciseScenarios from './components/exercise-scenarios/exerciseScenarios.js';
-import EmergencyResponseDocuments from './components/emergency-response-documents/emergencyResponseDocuments.js';
+import ExperienceLog from './components/experience-log/ExperienceLog';
+import Excercises from './components/exercise/Exercise';
+import ExcerciseScenarios from './components/exercise-scenarios/ExerciseScenarios';
+import EmergencyResponseDocuments from './components/emergency-response-documents/EmergencyResponseDocuments';
 import EmergencyResponseOrganization from './components/emergency-response-organization/EmergencyResponseOrganization';
-import CompetanceOverview from './components/competance-overview/competanceOverview';
-import EmergencyResponseDrawings from './components/emergency-response-drawings/emergencyResponseDrawings';
+import CompetenceOverview from './components/competence-overview/CompetenceOverview';
+import EmergencyResponseDrawings from './components/emergency-response-drawings/EmergencyResponseDrawings';
 
 
 class App extends Component {
@@ -26,7 +26,7 @@ class App extends Component {
           <Route path="/exercise-scenarios" component={ExcerciseScenarios} />
           <Route path="/emergency-response-documents" component={EmergencyResponseDocuments} />
           <Route path="/emergency-response-organization" component={EmergencyResponseOrganization} />
-          <Route path="/competance-overview" component={CompetanceOverview} />
+          <Route path="/competance-overview" component={CompetenceOverview} />
           <Route path="/emergency-response-drawings" component={EmergencyResponseDrawings} />
         </Switch>
       </Router>
