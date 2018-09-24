48152b35bc3fe08b6860ae11835e6e97551be081
Erlend Åmdal <erlend@aamdal.com>
2018-09-24 09:33:26 +0200

    Add icons for all emergency response organization subpages

--- src/components/emergency-response-organization/EmergencyResponseOrganization.js
+++ src/components/emergency-response-organization/EmergencyResponseOrganization.js
@@ -2,13 +2,17 @@ import React from 'react';
 import NavMenu from '../shared/NavMenu';
 import NavMenuItem from '../shared/NavMenuItem';
 import IconShield from '../../icons/shield.svg';
+import IconHierarchy from '../../icons/hierarchy.svg';
+import IconProfileGroup from '../../icons/profile-group.svg';
+import IconFileExcel from '../../icons/file-excel.svg';
+import IconCogs from '../../icons/cogs.svg';
 
 export default () => (
   <NavMenu>
-    <NavMenuItem name="Beredskapslag" icon={IconShield} to="" />
+    <NavMenuItem name="Beredskapslag" icon={IconHierarchy} to="" />
     <NavMenuItem name="Kompetansestatus" icon={IconShield} to="" />
-    <NavMenuItem name="Forenlighetsmatrise" icon={IconShield} to="" />
-    <NavMenuItem name="Stedfortredere" icon={IconShield} to="" />
-    <NavMenuItem name="Reserveressurser" icon={IconShield} to="" />
+    <NavMenuItem name="Forenlighetsmatrise" icon={IconFileExcel} to="" />
+    <NavMenuItem name="Stedfortredere" icon={IconProfileGroup} to="" />
+    <NavMenuItem name="Reserveressurser" icon={IconCogs} to="" />
   </NavMenu>
 );
