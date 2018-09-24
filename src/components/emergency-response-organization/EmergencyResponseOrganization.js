1918a632f0a86bfae15ce7b157f00dd61cb4c0c2
Erlend Åmdal <erlend@aamdal.com>
2018-09-24 10:22:12 +0200

    Apply a better icon for compatibility matrix

--- src/components/emergency-response-organization/EmergencyResponseOrganization.js
+++ src/components/emergency-response-organization/EmergencyResponseOrganization.js
@@ -3,15 +3,15 @@ import NavMenu from '../shared/NavMenu';
 import NavMenuItem from '../shared/NavMenuItem';
 import IconShield from '../../icons/shield.svg';
 import IconHierarchy from '../../icons/hierarchy.svg';
+import IconStats from '../../icons/stats-1.svg';
 import IconProfileGroup from '../../icons/profile-group.svg';
-import IconFileExcel from '../../icons/file-excel.svg';
 import IconCogs from '../../icons/cogs.svg';
 
 export default () => (
   <NavMenu>
     <NavMenuItem name="Beredskapslag" icon={IconHierarchy} to="emergency-response-teams/" />
     <NavMenuItem name="Kompetansestatus" icon={IconShield} to="competence-status/" />
-    <NavMenuItem name="Forenlighetsmatrise" icon={IconFileExcel} to="compatibility-matrix/" />
+    <NavMenuItem name="Forenlighetsmatrise" icon={IconStats} to="compatibility-matrix/" />
     <NavMenuItem name="Stedfortredere" icon={IconProfileGroup} to="deputies/" />
     <NavMenuItem name="Reserveressurser" icon={IconCogs} to="backup-resources/" />
   </NavMenu>
