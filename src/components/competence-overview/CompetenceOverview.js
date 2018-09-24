db4406dddd51afb622ccaa7c83f0476bdd0e12bd
Erlend Åmdal <erlend@aamdal.com>
2018-09-24 11:41:55 +0200

    Add Competence Overview page

--- src/components/competence-overview/CompetenceOverview.js
+++ src/components/competence-overview/CompetenceOverview.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import StatusIcon from '../shared/StatusIcon';
 import AppPage from '../shared/AppPage';
-import NavMenu from '../shared/NavMenu';
+import { teamList } from '../../dummy-data/team';
+
+const TeamItem = ({ team }) => (
+  <ListItem button>
+    <ListItemIcon style={{ fontSize: '40px' }}>
+      <StatusIcon status={team.status} />
+    </ListItemIcon>
+    <ListItemText primary={team.name} />
+  </ListItem>
+);
 
 export default () => (
   <AppPage title="Kompetanseoversikt">
-    <NavMenu>
-    </NavMenu>
+    <List>
+      {teamList.map(team => (
+        <TeamItem key={team.id} team={team} />
+      ))}
+    </List>
   </AppPage>
 );
