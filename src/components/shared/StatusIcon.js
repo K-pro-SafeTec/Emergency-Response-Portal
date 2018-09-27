4d07ec1f39bbfdd3796588d263f8578f41e28b3b
wfeiring <wfeiring@gmail.com>
2018-09-27 16:09:44 +0200

    Edited alt text and other img tag props

--- src/components/shared/StatusIcon.js
+++ src/components/shared/StatusIcon.js
@@ -7,9 +7,9 @@ import Checkmark from '../../icons/livefeed/checkmark.svg';
 import Error from '../../icons/livefeed/not-checkmark.svg';
 
 const statusIcons = new Map([
-  [Status.OK, (props) => <img src={Checkmark} alt="Ok" heigth="35em" width="35em"{...props} />],
-  [Status.WARNING, (props) => <img src={WarningYellow} alt="Advarsel" heigth="35em" width="35em"{...props} />],
-  [Status.ERROR, (props) => <img src={Error} alt="OBS!" heigth="35em" width="35em"{...props} />],
+  [Status.OK, (props) => <img src={Checkmark} alt="Ok" height="35em" width="35em" {...props} />],
+  [Status.WARNING, (props) => <img src={WarningYellow} alt="Advarsel" height="35em" width="35em" {...props} />],
+  [Status.ERROR, (props) => <img src={Error} alt="OBS!" height="35em" width="35em" {...props} />],
 ]);
 
 export default ({ status, ...rest }) => {
