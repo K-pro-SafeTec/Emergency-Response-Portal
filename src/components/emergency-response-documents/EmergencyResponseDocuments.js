8f59a4afa417cc9cb73619b079b0d37ec5264c84
Erlend Åmdal <erlend@aamdal.com>
2018-09-27 13:10:46 +0200

    Add tables for documents and drawings

--- src/components/emergency-response-documents/EmergencyResponseDocuments.js
+++ src/components/emergency-response-documents/EmergencyResponseDocuments.js
@@ -1,10 +1,78 @@
 import React from 'react';
 import AppPage from '../shared/AppPage';
-import NavMenu from '../shared/NavMenu';
+import DocumentTable from '../shared/DocumentTable';
+
+const documents = [
+  {
+    id: 0,
+    rev: '4.0',
+    date: 'xx.xx.xxxx',
+    title: 'Beredskapsplan',
+  },
+  {
+    id: 1,
+    rev: '2.0',
+    date: 'xx.xx.xxxx',
+    title: 'Beredskapsanalyse',
+  },
+  {
+    id: 2,
+    rev: '4.0',
+    date: 'xx.xx.xxxx',
+    title: 'Varslingsmatrise',
+  },
+  {
+    id: 3,
+    rev: '11.0',
+    date: 'xx.xx.xxxx',
+    title: 'Telefonliste',
+  },
+  {
+    id: 4,
+    rev: '2.0',
+    date: 'xx.xx.xxxx',
+    title: 'Alarminstruks',
+  },
+  {
+    id: 5,
+    rev: '2.0',
+    date: 'xx.xx.xxxx',
+    title: 'Dimensjoneringsanalyse',
+  },
+  {
+    id: 6,
+    rev: '5.0',
+    date: 'xx.xx.xxxx',
+    title: 'Styrende dokument beredskap',
+  },
+  {
+    id: 7,
+    rev: '6.0',
+    date: 'xx.xx.xxxx',
+    title: 'Innretningsspesifikke føringer',
+  },
+  {
+    id: 8,
+    rev: '3.0',
+    date: 'xx.xx.xxxx',
+    title: 'Risikoanalyse',
+  },
+  {
+    id: 9,
+    rev: '2.0',
+    date: 'xx.xx.xxxx',
+    title: 'Områdeberedskapsplan',
+  },
+  {
+    id: 10,
+    rev: '3.0',
+    date: 'xx.xx.xxxx',
+    title: 'Oljevernplan',
+  },
+];
 
 export default () => (
   <AppPage title="Beredskapsdokumenter">
-    <NavMenu>
-    </NavMenu>
+    <DocumentTable documents={documents} />
   </AppPage>
 );
--- src/components/emergency-response-drawings/EmergencyResponseDrawings.js
+++ src/components/emergency-response-drawings/EmergencyResponseDrawings.js
@@ -1,10 +1,48 @@
 import React from 'react';
 import AppPage from '../shared/AppPage';
-import NavMenu from '../shared/NavMenu';
+import DocumentTable from '../shared/DocumentTable';
+
+const documents = [
+  {
+    id: 0,
+    rev: '4.0',
+    date: 'xx.xx.xxxx',
+    title: 'Fire and escape plan',
+  },
+  {
+    id: 1,
+    rev: '2.0',
+    date: 'xx.xx.xxxx',
+    title: 'Subsea installasjoner',
+  },
+  {
+    id: 2,
+    rev: '4.0',
+    date: 'xx.xx.xxxx',
+    title: 'Plot planer',
+  },
+  {
+    id: 3,
+    rev: '11.0',
+    date: 'xx.xx.xxxx',
+    title: 'Detektor layout',
+  },
+  {
+    id: 4,
+    rev: '2.0',
+    date: 'xx.xx.xxxx',
+    title: 'Førstehjelpsutstyr',
+  },
+  {
+    id: 4,
+    rev: '1.0',
+    date: 'xx.xx.xxxx',
+    title: 'Innretninger i nærheten',
+  },
+];
 
 export default () => (
-  <AppPage title="Beredskapsdokumenter">
-    <NavMenu>
-    </NavMenu>
+  <AppPage title="Beredskapsrelaterte tegninger">
+    <DocumentTable documents={documents} />
   </AppPage>
 );
