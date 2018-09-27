c49e48b574e9880222492db64a4865be9d8268cb
Erlend Åmdal <erlend@aamdal.com>
2018-09-27 16:32:35 +0200

    Add deputies

--- src/components/emergency-response-organization/deputies/Deputies.js
+++ src/components/emergency-response-organization/deputies/Deputies.js
@@ -1,3 +1,39 @@
 import React from 'react';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import AppPage from '../../shared/AppPage';
+import { roleList, roleById } from '../../../dummy-data/role';
+import { personById } from '../../../dummy-data/person';
 
-export default () => <p>Placeholder</p>;
+export default () => (
+  <AppPage title="Stedfortredere">
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>Beredskapsrolle</TableCell>
+          <TableCell>Stedfortrederrolle</TableCell>
+          <TableCell>Stedfortredere</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {
+          roleList
+            .filter(role => role.replacementRole !== null)
+            .map(role => {
+              const replacementRole = roleById[role.replacementRole];
+              return (
+                <TableRow key={role.id}>
+                  <TableCell>{role.name}</TableCell>
+                  <TableCell>{replacementRole.name}</TableCell>
+                  <TableCell>{personById[replacementRole.person].name}</TableCell>
+                </TableRow>
+              );
+            })
+        }
+      </TableBody>
+    </Table>
+  </AppPage>
+);
