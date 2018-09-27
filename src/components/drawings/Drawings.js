cc90463df00de78d94a5942c8e6cb4e16b380b73
Erlend Åmdal <erlend@aamdal.com>
2018-09-27 15:06:56 +0200

    Separate document data to dummy data

--- src/components/drawings/Drawings.js
+++ src/components/drawings/Drawings.js
@@ -1,48 +1,10 @@
 import React from 'react';
 import AppPage from '../shared/AppPage';
 import DocumentTable from '../shared/DocumentTable';
-
-const documents = [
-  {
-    id: 0,
-    rev: '4.0',
-    date: 'xx.xx.xxxx',
-    title: 'Fire and escape plan',
-  },
-  {
-    id: 1,
-    rev: '2.0',
-    date: 'xx.xx.xxxx',
-    title: 'Subsea installasjoner',
-  },
-  {
-    id: 2,
-    rev: '4.0',
-    date: 'xx.xx.xxxx',
-    title: 'Plot planer',
-  },
-  {
-    id: 3,
-    rev: '11.0',
-    date: 'xx.xx.xxxx',
-    title: 'Detektor layout',
-  },
-  {
-    id: 4,
-    rev: '2.0',
-    date: 'xx.xx.xxxx',
-    title: 'Førstehjelpsutstyr',
-  },
-  {
-    id: 4,
-    rev: '1.0',
-    date: 'xx.xx.xxxx',
-    title: 'Innretninger i nærheten',
-  },
-];
+import { drawingList } from '../../dummy-data/drawing';
 
 export default () => (
   <AppPage title="Beredskapsrelaterte tegninger">
-    <DocumentTable documents={documents} />
+    <DocumentTable documents={drawingList} />
   </AppPage>
 );
