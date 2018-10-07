5e00f54aa45dfe2cd25a87d550fa1607a53ae835
Maria <maria@soleim.com>
2018-10-07 22:52:21 +0200

    Moved data to data folder

--- src/components/exercise-scenarios/ExerciseScenarios.js
+++ src/components/exercise-scenarios/ExerciseScenarios.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import AppPage from "../shared/AppPage";
+import scenarios from "./../../dummy-data/exercise-scenarios"
 import Select from '@material-ui/core/Select';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
@@ -9,70 +10,10 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import MenuItem from "@material-ui/core/MenuItem/MenuItem";
 
-const scenarios = [
-  {
-    id: 0,
-    week: 2,
-    type: 'Øvelse',
-    dfu: 4,
-    scenario: 'Fartøy på kollisjonskurs'
-  },
-  {
-    id: 1,
-    week: 3,
-    type: 'Table top',
-    dfu: 15,
-    scenario: 'Ekstremvær over design-/operasjonskriterier'
-  },
-  {
-    id: 2,
-    week: 8,
-    type: 'Øvelse',
-    dfu: 11,
-    scenario: 'Alvorlig syk/skadet person'
-  },
-  {
-    id: 3,
-    week: 9,
-    type: 'Table top',
-    dfu: 7,
-    scenario: 'Helikopterulykke i sjø'
-  },
-  {
-    id: 4,
-    week: 14,
-    type: 'Øvelse',
-    dfu: 10,
-    scenario: 'Mann over bord'
-  },
-  {
-    id: 5,
-    week: 15,
-    type: 'Table top',
-    dfu: '12',
-    scenario: 'Sikringstrussel/terror'
-  },
-  {
-    id: 6,
-    week: 20,
-    type: 'Øvelse',
-    dfu: 9,
-    scenario: 'Brann i boligkvarter'
-  },
-  {
-    id: 7,
-    week: 21,
-    type: 'Table top',
-    dfu: 14,
-    scenario: 'Strukturhendelse'
-  }
-];
-
-
 export default class ExerciseScenarios extends React.Component {
 
   constructor(props) {
-    super(props)
+    super(props);
     this.state ={
       sortingCategory: 'week'
     }
---
+++ src/dummy-data/exercise-scenarios.js
@@ -0,0 +1,58 @@
+export default [
+  {
+    id: 0,
+    week: 2,
+    type: 'Øvelse',
+    dfu: 4,
+    scenario: 'Fartøy på kollisjonskurs'
+  },
+  {
+    id: 1,
+    week: 3,
+    type: 'Table top',
+    dfu: 15,
+    scenario: 'Ekstremvær over design-/operasjonskriterier'
+  },
+  {
+    id: 2,
+    week: 8,
+    type: 'Øvelse',
+    dfu: 11,
+    scenario: 'Alvorlig syk/skadet person'
+  },
+  {
+    id: 3,
+    week: 9,
+    type: 'Table top',
+    dfu: 7,
+    scenario: 'Helikopterulykke i sjø'
+  },
+  {
+    id: 4,
+    week: 14,
+    type: 'Øvelse',
+    dfu: 10,
+    scenario: 'Mann over bord'
+  },
+  {
+    id: 5,
+    week: 15,
+    type: 'Table top',
+    dfu: '12',
+    scenario: 'Sikringstrussel/terror'
+  },
+  {
+    id: 6,
+    week: 20,
+    type: 'Øvelse',
+    dfu: 9,
+    scenario: 'Brann i boligkvarter'
+  },
+  {
+    id: 7,
+    week: 21,
+    type: 'Table top',
+    dfu: 14,
+    scenario: 'Strukturhendelse'
+  }
+];
