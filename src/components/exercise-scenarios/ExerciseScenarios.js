c28e44a322c8f0fd247dd4118b00f392e4c7fe79
Maria <maria@soleim.com>
2018-10-08 13:09:09 +0200

    Sorting by clicking on columns

--- src/components/exercise-scenarios/ExerciseScenarios.js
+++ src/components/exercise-scenarios/ExerciseScenarios.js
@@ -1,77 +1,170 @@
 import React from 'react';
 import AppPage from "../shared/AppPage";
 import scenarios from "./../../dummy-data/exercise-scenarios"
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import MenuItem from "@material-ui/core/MenuItem/MenuItem";
-import InputLabel from "@material-ui/core/InputLabel/InputLabel";
+import Tooltip from "@material-ui/core/Tooltip/Tooltip";
+import TableSortLabel from "@material-ui/core/TableSortLabel/TableSortLabel";
 
-export default class ExerciseScenarios extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      sortingCategory: ''
-    }
+function desc(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
   }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
 
-  handleChange = event => {
-    this.setState({ sortingCategory: event.target.value });
+function stableSort(array, cmp) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = cmp(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map(el => el[0]);
+}
+
+function getSorting(order, orderBy) {
+  return order === "desc"
+    ? (a, b) => desc(a, b, orderBy)
+    : (a, b) => -desc(a, b, orderBy);
+}
+
+const rows = [
+  { id: "week", numeric: true, label: "Uke" },
+  { id: "type", numeric: false, label: "Type" },
+  { id: "dfu", numeric: true, label: "DFU" },
+  { id: "scenario", numeric: false, label: "Scenario" },
+];
+
+class EnhancedTableHead extends React.Component {
+  createSortHandler = property => event => {
+    this.props.onRequestSort(event, property);
   };
 
   render() {
+    const { order, orderBy } = this.props;
+
+    return (
+      <TableHead>
+        <TableRow>
+          {rows.map(row => {
+            return (
+              <TableCell
+                key={row.id}
+                numeric={row.numeric}
+                padding={row.disablePadding ? "none" : "default"}
+                sortDirection={orderBy === row.id ? order : false}
+              >
+                <Tooltip
+                  title="Sortér"
+                  placement={row.numeric ? "bottom-end" : "bottom-start"}
+                  enterDelay={300}
+                >
+                  <TableSortLabel
+                    active={orderBy === row.id}
+                    direction={order}
+                    onClick={this.createSortHandler(row.id)}
+                  >
+                    {row.label}
+                  </TableSortLabel>
+                </Tooltip>
+              </TableCell>
+            );
+          }, this)}
+        </TableRow>
+      </TableHead>
+    );
+  }
+}
+
+export default class EnhancedTable extends React.Component {
+  state = {
+    order: "asc",
+    orderBy: "week",
+    data: scenarios,
+  };
+
+  handleRequestSort = (event, property) => {
+    const orderBy = property;
+    let order = "desc";
+    if (this.state.orderBy === property && this.state.order === "desc") {
+      order = "asc";
+    }
+    this.setState({ order, orderBy });
+  };
+
+  handleClick = (event, id) => {
+    const { selected } = this.state;
+    const selectedIndex = selected.indexOf(id);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, id);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1)
+      );
+    }
+
+    this.setState({ selected: newSelected });
+  };
+
+  render() {
+    const { data, order, orderBy, rowsPerPage, page } = this.state;
+    const emptyRows =
+      rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage);
+
     return (
       <AppPage title="Øvelsesscenarioer" back="..">
-        <div style={{display: 'flex'}}>
-          <div style={{flex: '4'}}/>
-          <div style={{flex: '1', marginRight: '1em', marginTop: '1em'}}>
-            <FormControl style={{width: '100%'}}>
-              <InputLabel>Sortér etter</InputLabel>
-              <Select
-                value={this.state.sortingCategory}
-                onChange={this.handleChange}
-              >
-                <MenuItem value={"week"}>Uke</MenuItem>
-                <MenuItem value={"type"}>Type</MenuItem>
-                <MenuItem value={"dfu"}>DFU</MenuItem>
-                <MenuItem value={"scenario"}>Scenario</MenuItem>
-              </Select>
-            </FormControl>
-          </div>
-        </div>
-        <Paper style={{margin: '1em'}}>
-          <Table>
-            <TableHead>
-                <TableRow>
-                <TableCell>Uke</TableCell>
-                <TableCell>Type</TableCell>
-                <TableCell>DFU</TableCell>
-                <TableCell>Scenario</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {[].concat(scenarios)
-                .sort((a, b) => (a[this.state.sortingCategory] > b[this.state.sortingCategory] ? 1 : 0))
-                .map(row => {
-                return (
-                  <TableRow key={row.id}>
-                    <TableCell>{row.week}</TableCell>
-                    <TableCell>{row.type}</TableCell>
-                    <TableCell>{row.dfu}</TableCell>
-                    <TableCell>{row.scenario}</TableCell>
+        <Paper>
+          <div>
+            <Table>
+              <EnhancedTableHead
+                order={order}
+                orderBy={orderBy}
+                onRequestSort={this.handleRequestSort}
+                rowCount={data.length}
+              />
+              <TableBody>
+                {stableSort(data, getSorting(order, orderBy))
+                  .map(n => {
+                    return (
+                      <TableRow
+                        hover
+                        onClick={event => this.handleClick(event, n.id)}
+                        role="checkbox"
+                        tabIndex={-1}
+                        key={n.id}
+                      >
+                        <TableCell numeric>{n.week}</TableCell>
+                        <TableCell>{n.type}</TableCell>
+                        <TableCell numeric>{n.dfu}</TableCell>
+                        <TableCell>{n.scenario}</TableCell>
+                      </TableRow>
+                    );
+                  })}
+                {emptyRows > 0 && (
+                  <TableRow style={{ height: 49 * emptyRows }}>
+                    <TableCell colSpan={6} />
                   </TableRow>
-                );
-              })}
-            </TableBody>
-          </Table>
+                )}
+              </TableBody>
+            </Table>
+          </div>
         </Paper>
       </AppPage>
-    )
+    );
   }
 }
