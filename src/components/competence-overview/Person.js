ccf63fb2f1d10fac4600e778b0c2d1d60303bd43
Erlend Åmdal <erlend@aamdal.com>
2018-10-08 10:03:59 +0200

    Add basic person info to competence overview

--- src/components/competence-overview/Person.js
+++ src/components/competence-overview/Person.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { teamById } from '../../dummy-data/team';
 import { personById } from '../../dummy-data/person';
+import { roleById } from '../../dummy-data/role';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+import Typography from '@material-ui/core/Typography';
+import PersonIcon from '@material-ui/icons/Person';
 import StatusIcon from '../shared/StatusIcon';
 import AppPage from '../shared/AppPage';
 import Status, { mostSevere } from '../../helpers/Status';
+import { withStyles } from '@material-ui/core';
 
 const TeamItem = ({ team, person }) => (
   <ListItem button component={Link} to={`../../teams/${team.id}/`}>
@@ -25,12 +29,41 @@ const TeamItem = ({ team, person }) => (
   </ListItem>
 );
 
-export default ({ match }) => {
+const styles = {
+  personInfoContainer: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    padding: '1em',
+  },
+  personAvatar: {
+    height: '4em',
+    flex: '0 0 4em',
+  },
+  personInfo: {
+    flex: '1 1 24em',
+    padding: '1em',
+  },
+};
+
+const Person = ({ classes, match }) => {
   const person = personById[match.params.personId];
   if (person) {
     return (
       <AppPage title={`Kompetanseoversikt - ${person.name}`} back="../..">
+        <div className={classes.personInfoContainer}>
+          <PersonIcon className={classes.personAvatar} />
+          <div className={classes.personInfo}>
+            <Typography variant="headline">{person.name}</Typography>
+            <Typography variant="subheading">
+              {person.roles.map(role => roleById[role].name).join(", ")}
+            </Typography>
+          </div>
+        </div>
         <List>
+          <ListItem>
+            <Typography variant="title">Beredskapslag</Typography>
+          </ListItem>
           {person.teams.map(teamId => (
             <TeamItem key={teamId} team={teamById[teamId]} person={person} />
           ))}
@@ -40,3 +73,5 @@ export default ({ match }) => {
   }
   return <p>Fant ikke noen person med denne IDen.</p>
 };
+
+export default withStyles(styles)(Person);
