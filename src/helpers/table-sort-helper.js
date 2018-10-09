a5dc21839573ef34dfc8f5b54686deba3399d4ae
Maria <maria@soleim.com>
2018-10-10 00:07:05 +0200

    Revert "Works"
    This reverts commit 066757c55d1fe2c1896773a3d5b2e4744dc9d2cb.

--- src/helpers/table-sort-helper.js
+++ src/helpers/table-sort-helper.js
@@ -8,25 +8,6 @@ function desc(a, b, orderBy) {
   return 0;
 }
 
-function statusDesc(a, b, competenceNumber) {
-  const statuses = ["Symbol(OK)", "Symbol(WARNING)", "Symbol(ERROR)"];
-  let indexA = 2;
-  let indexB = 2;
-  if (a.competence[competenceNumber]) {
-    indexA = statuses.indexOf(a.competence[competenceNumber].status.toString());
-  }
-  if (b.competence[competenceNumber]) {
-    indexB = statuses.indexOf(b.competence[competenceNumber].status.toString());
-  }
-  if (indexB < indexA) {
-    return -1;
-  }
-  if (indexB > indexA) {
-    return 1;
-  }
-  return 0;
-}
-
 export function stableSort(array, cmp) {
   const stabilizedThis = array.map((el, index) => [el, index]);
   stabilizedThis.sort((a, b) => {
@@ -38,8 +19,7 @@ export function stableSort(array, cmp) {
 }
 
 export function getSorting(order, orderBy) {
-  const descFunction = (((typeof orderBy) === "number") ? statusDesc : desc);
   return order === "desc"
-    ? (a, b) => descFunction(a, b, orderBy)
-    : (a, b) => -descFunction(a, b, orderBy);
+    ? (a, b) => desc(a, b, orderBy)
+    : (a, b) => -desc(a, b, orderBy);
 }
