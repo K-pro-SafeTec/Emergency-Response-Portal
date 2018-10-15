aa1944eb9921f7f8f6c25c2bd61afdd3b83811ba
Erlend Åmdal <erlend@aamdal.com>
2018-10-15 13:24:44 +0200

    Add document placeholder view

--- src/components/documents/Documents.js
+++ src/components/documents/Documents.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core';
@@ -6,6 +7,7 @@ import AppPage from '../shared/AppPage';
 import { documentList } from '../../dummy-data/document';
 import { drawingList } from '../../dummy-data/drawing';
 import DocumentTableContainer from "./DocumentTableContainer";
+import DocumentView from "./DocumentView";
 
 
 const styles = {
@@ -20,29 +22,34 @@ const styles = {
   },
 };
 
-const Documents = ({ classes }) => (
-  <AppPage title="Beredskapsdokumenter" back="..">
-    <div className={classes.tableContainer}>
-      <Paper className={classes.table}>
-        <Typography
-          variant="headline"
-          align="center"
-        >
-          Dokumenter
-        </Typography>
-        <DocumentTableContainer documents={documentList} />
-      </Paper>
-      <Paper className={classes.table}>
-        <Typography
-          variant="headline"
-          align="center"
-        >
-          Tegninger
-        </Typography>
-        <DocumentTableContainer documents={drawingList} />
-      </Paper>
-    </div>
-  </AppPage>
+const Documents = ({ classes, match }) => (
+  <Switch>
+    <Route exact path={match.path}>
+      <AppPage title="Beredskapsdokumenter" back="..">
+        <div className={classes.tableContainer}>
+          <Paper className={classes.table}>
+            <Typography
+              variant="headline"
+              align="center"
+            >
+              Dokumenter
+            </Typography>
+            <DocumentTableContainer documents={documentList} />
+          </Paper>
+          <Paper className={classes.table}>
+            <Typography
+              variant="headline"
+              align="center"
+            >
+              Tegninger
+            </Typography>
+            <DocumentTableContainer documents={drawingList} />
+          </Paper>
+        </div>
+      </AppPage>
+    </Route>
+    <Route path={`${match.url}/:documentId/`} component={DocumentView} />
+  </Switch>
 );
 
 export default withStyles(styles)(Documents);
