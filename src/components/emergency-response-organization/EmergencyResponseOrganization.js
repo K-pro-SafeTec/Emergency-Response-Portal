c384af518c8d012dd4c058cfa32fb37b9da8dd0b
Erlend Åmdal <erlend@aamdal.com>
2018-10-18 10:27:52 +0200

    Remove competence status

--- src/components/emergency-response-organization/EmergencyResponseOrganization.js
+++ src/components/emergency-response-organization/EmergencyResponseOrganization.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import NavMenu from '../shared/NavMenu';
 import NavMenuItem from '../shared/NavMenuItem';
-import IconCompetanceStatus from '../../icons/emergency-response-organization/competance-status.svg';
 import IconResponseTeams from '../../icons/emergency-response-organization/response-teams.svg';
 import IconStats from '../../icons/emergency-response-organization/stats-1.svg';
 import IconDeputies from '../../icons/emergency-response-organization/deputies.svg';
@@ -9,7 +8,6 @@ import IconCogs from '../../icons/emergency-response-organization/cogs.svg';
 import AppPage from '../shared/AppPage';
 import BackupResources from './backup-resources/BackupResources';
 import CompatibilityMatrix from './compatibility-matrix/CompatibilityMatrix';
-import CompetenceStatus from './competence-status/CompetenceStatus';
 import DeputiesContainer from './deputies/DeputiesContainer';
 import EmergencyResponseTeams from './emergency-response-teams/EmergencyResponseTeams';
 import { Switch, Route } from 'react-router-dom';
@@ -20,7 +18,6 @@ export default ({ match }) => (
       <AppPage title="Beredskapsorganisasjon" back="..">
         <NavMenu>
           <NavMenuItem name="Beredskapslag" icon={IconResponseTeams} to="emergency-response-teams/" />
-          <NavMenuItem name="Kompetansestatus" icon={IconCompetanceStatus} to="competence-status/" />
           <NavMenuItem name="Forenlighetsmatrise" icon={IconStats} to="compatibility-matrix/" />
           <NavMenuItem name="Stedfortredere" icon={IconDeputies} to="deputies/" />
           <NavMenuItem name="Reserveressurser" icon={IconCogs} to="backup-resources/" />
@@ -29,7 +26,6 @@ export default ({ match }) => (
     </Route>
     <Route path={`${match.path}backup-resources/`} component={BackupResources} />
     <Route path={`${match.path}compatibility-matrix/`} component={CompatibilityMatrix} />
-    <Route path={`${match.path}competence-status/`} component={CompetenceStatus} />
     <Route path={`${match.path}deputies/`} component={DeputiesContainer} />
     <Route path={`${match.path}emergency-response-teams/`} component={EmergencyResponseTeams} />
   </Switch>
