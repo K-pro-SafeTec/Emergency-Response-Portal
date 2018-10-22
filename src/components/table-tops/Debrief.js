ccf3a03da87a07d96f21bebfbf3eac2d0372b1c8
Erlend Åmdal <erlend@aamdal.com>
2018-10-22 15:43:58 +0200

    Use placeholder pages for debriefs

--- src/components/table-tops/Debrief.js
+++ src/components/table-tops/Debrief.js
@@ -1,7 +1,9 @@
 import React  from 'react';
+import PlaceholderPage from '../shared/PlaceholderPage';
 
-export default ({match}) => (
-  <div>
-    <h2>Debrief report for scenario {match.params.scenarioId} on date {match.params.date}.</h2>
-  </div>
+
+export default ({ match }) => (
+  <PlaceholderPage title="Rapport">
+    {/* <h2>Debrief report for scenario {match.params.scenarioId} on date {match.params.date}.</h2> */}
+  </PlaceholderPage>
 )
