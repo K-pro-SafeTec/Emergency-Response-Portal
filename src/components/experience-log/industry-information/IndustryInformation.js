f1fc3a6e4f1e9cbb1119d8168eb9dcfa72d17f6b
Erlend Åmdal <erlend@aamdal.com>
2018-10-22 09:19:43 +0200

    Add industry information

--- src/components/experience-log/industry-information/IndustryInformation.js
+++ src/components/experience-log/industry-information/IndustryInformation.js
@@ -1,5 +1,52 @@
 import React from 'react';
-import PlaceholderPage from '../../shared/PlaceholderPage';
+import AppPage from '../../shared/AppPage';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import { industryInformationList } from '../../../dummy-data/industryInformation';
 
 
-export default () => <PlaceholderPage title="Bransjeinformasjon" back=".." />;
+const IndustryInformation = () => (
+  <AppPage title="Bransjeinformasjon" back="..">
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>
+            ID
+          </TableCell>
+          <TableCell>
+            Dato
+          </TableCell>
+          <TableCell>
+            Tema
+          </TableCell>
+          <TableCell>
+            Informasjon
+          </TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {industryInformationList.map(industryInformation => (
+          <TableRow key={industryInformation.id}>
+            <TableCell>
+              {industryInformation.id}
+            </TableCell>
+            <TableCell>
+              {industryInformation.date}
+            </TableCell>
+            <TableCell>
+              {industryInformation.subject}
+            </TableCell>
+            <TableCell>
+              {industryInformation.content}
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </AppPage>
+);
+
+export default IndustryInformation;
---
+++ src/dummy-data/industryInformation.js
@@ -0,0 +1,26 @@
+export const industryInformationList = [
+  {
+    id: 0,
+    date: "2018-03-06",
+    subject: "Trening beredskapslag, Ptil",
+    content: "Ved tilsyn i det siste har Ptil hatt fokus på loggføring av trening av beredskapslag. De ønsker status på pseronell som er om bord mht. gjennomførte treninger, samt totaloversikt over status på treninger på mannskapet."
+  },
+  {
+    id: 1,
+    date: "2018-05-07",
+    subject: "Skytteltanker, black-out",
+    content: "En skytteltanker som var oppkoblet til BW Azurite på kysten av indonesia fikk black-out og drev mot FPSOen. Skytteltanker gjenvant kraftforsyningen etter 89 minutter og kollisjon ble avverget."
+  },
+  {
+    id: 2,
+    date: "2018-07-22",
+    subject: "Brann i hjelpesystemer, black-out",
+    content: "Ved brann i generator på en innretning ble tavler i tilstøtende rom koblet ut. Det viste seg at nødstrøm, ved en feil, var koblet til en av disse tavlene og innretnigen mistet all strømforsyning i en kortere periode."
+  },
+  {
+    id: 3,
+    date: "2018-03-08",
+    subject: "SAR-helikopter, områderessurser",
+    content: "En innretning dekket av områdeberedskapen på Tampen gjennomførte arbeid over sjø samtidig som SAR-helikopteret på Tampen var på vei onshore med MEDEVAC. Innretningen hadde mottat varsel per e-post, men det tok 67 minutter før e-posten ble lest. Arbeid over sjø ble da stanses inntil SAR-helikopteret var på feltet igjen."
+  }
+];
