156eca7812e004b01b6bced01fe35634354ca334
Maria <maria@soleim.com>
2018-10-25 09:23:48 +0200

    Removed test

--- src/App.test.js
+++ src/App.test.js
@@ -1,31 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import EmergencyResponsePortalCalendar from "./components/calendar/Calendar";
-import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
-});
-
-test('Link changes the class when hovered', () => {
-  const component = renderer.create(
-    <EmergencyResponsePortalCalendar />,
-  );
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-
-  // manually trigger the callback
-  tree.props.onMouseEnter();
-  // re-rendering
-  tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-
-  // manually trigger the callback
-  tree.props.onMouseLeave();
-  // re-rendering
-  tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
 });
