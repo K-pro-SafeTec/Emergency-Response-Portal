13fd662beabab24be1ba292b0822230d6f34a3a8
Erlend Åmdal <erlend@aamdal.com>
2018-10-25 08:48:06 +0200

    Prettier ER teams

--- src/components/emergency-response-organization/emergency-response-teams/EmergencyResponseTeams.js
+++ src/components/emergency-response-organization/emergency-response-teams/EmergencyResponseTeams.js
@@ -2,22 +2,25 @@ import React from 'react';
 import AppPage from "../../shared/AppPage";
 import { teamList } from "../../../dummy-data/team";
 import Team from "./Team"
+import { withStyles } from '@material-ui/core';
 
 const styles = {
-  display: 'flex',
-  flexDirection: 'row',
-  flexWrap: 'wrap',
-  justifyContent: 'center'
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
 };
 
-const EmergencyResponseTeams = () => (
+const EmergencyResponseTeams = ({ classes }) => (
   <AppPage title="Beredskapslag" back="..">
-    <div style={styles}>
-        {teamList.map(team => (
-          <Team key={team.id} team={team} />
-        ))}
+    <div className={classes.container}>
+      {teamList.map(team => (
+        <Team key={team.id} team={team} />
+      ))}
     </div>
   </AppPage>
 );
 
-export default EmergencyResponseTeams;
+export default withStyles(styles)(EmergencyResponseTeams);
--- src/components/emergency-response-organization/emergency-response-teams/Person.js
+++ src/components/emergency-response-organization/emergency-response-teams/Person.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ListItem from "@material-ui/core/ListItem/ListItem";
+import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from "@material-ui/core/ListItemText/ListItemText";
 import PersonIcon from '@material-ui/icons/Person';
 
-const Person = ({name}) => (
+const Person = ({ name }) => (
   <ListItem>
-    <PersonIcon/>
+    <ListItemIcon>
+      <PersonIcon/>
+    </ListItemIcon>
     <ListItemText primary={name} />
   </ListItem>
 );
