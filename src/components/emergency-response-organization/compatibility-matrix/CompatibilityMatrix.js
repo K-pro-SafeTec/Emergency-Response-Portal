bf44ef03b3423f2f7547ca6b32f006450a40efef
snolbo <snorrebortnes@gmail.com>
2018-10-29 14:38:00 +0100

    more edits

--- src/components/emergency-response-organization/compatibility-matrix/CompatibilityMatrix.js
+++ src/components/emergency-response-organization/compatibility-matrix/CompatibilityMatrix.js
@@ -3,7 +3,6 @@ import AppPage from '../../shared/AppPage';
 
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
-import CardMedia from '@material-ui/core/CardMedia';
 import IllustrationCompatibilityMatrix from '../../../illustrations/comptatibility-matrix.svg';
 
 import compat_image from './compat_matrix.png';
@@ -21,11 +20,11 @@ function MediaCard(props) {
   <AppPage title="Forenlighetsmatrise" back="..">
     <img
         src={IllustrationCompatibilityMatrix}
-        alt="Emergency response team hierarchy"
+        alt="Beredskapslag hierarki"
     />
     <img
         src={compat_image}
-        alt="Compatability matrix"
+        alt="Forenlighetsmatrise"
     />
   </AppPage>
 );
