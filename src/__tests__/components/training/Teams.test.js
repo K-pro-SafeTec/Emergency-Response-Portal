a0536d3e1d0b9e9a09487fbcb01bb04753a2e0ea
Maria <maria@soleim.com>
2018-10-30 18:59:15 +0100

    training teams

--- src/__tests__/components/training/Teams.test.js
+++ src/__tests__/components/training/Teams.test.js
@@ -2,13 +2,14 @@ import React from 'react';
 import TestRenderer from 'react-test-renderer';
 import { MemoryRouter as Router } from 'react-router-dom'
 import AppPage from "../../../components/shared/AppPage";
-import ExperienceLog from "../../../components/experience-log/ExperienceLog";
+import Teams from "../../../components/training/Teams";
+import ListItem from "@material-ui/core/ListItem/ListItem";
 
 let testRenderer;
 let testInstance;
 
 beforeAll(() => {
-  testRenderer = TestRenderer.create(<Router><ExperienceLog match={{params: {personId: 0}}} /></Router>);
+    testRenderer = TestRenderer.create(<Router><Teams /></Router>);
   testInstance = testRenderer.root;
 });
 
@@ -18,5 +19,9 @@ test('renders correctly', () => {
 });
 
 test('title is correct', () => {
-  expect(testInstance.findByType(AppPage).props.title).toBe("Erfaringslogg");
+  expect(testInstance.findByType(AppPage).props.title).toBe("Trening");
+});
+
+test('nav menu contains all items', () => {
+  expect(testInstance.findAllByType(ListItem).length).toBe(1+5);
 });
