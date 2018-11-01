bdb3eed8a350563676e25ad53313d5c274e16391
Maria <maria@soleim.com>
2018-11-01 09:05:33 +0100

    Deleted training debrief and unused exercise

--- src/components/training/Debrief.js
+++
@@ -1,8 +0,0 @@
-import React  from 'react';
-import PlaceholderPage from '../shared/PlaceholderPage';
-
-export default ({ match }) => (
-  <PlaceholderPage title="Rapport">
-    {/* <h2>Debrief report for scenario {match.params.scenarioId} on date {match.params.date}.</h2> */}
-  </PlaceholderPage>
-)
